907f051523374e39326839ae472ae5bc580a25da
Marc DELOMEZ <marc.delomez@free.fr>
2019-04-20 17:19:26 +0200

    add title props for contextmenu ctrl

--- src/lib/controls/contextmenu.js
+++ src/lib/controls/contextmenu.js
@@ -1,19 +1,35 @@
 $$.control.registerControl('brainjs.contextmenu', {
 	props: {
 		items: {},
-		trigger: 'right'
+		trigger: 'right',
+		className: 'data-title',
+		title: ''
 	},
 	init: function(elt) {
 
-		var id = elt.attr('id')
+		const {items, trigger, className, title} = this.props
+
+		const id = elt.attr('id')
 		$.contextMenu({
-			trigger: this.props.trigger,
+			//appendTo: elt.get(0),
+			trigger,
+			className: 'data-title ' + id,
 			selector: '#' + id,
 			callback: function(cmd) {
 				elt.trigger('contextmenuchange', {cmd})
 			},
-			items: this.props.items
+			items
 		})	
+
+		if (title != '') {
+			$('.' + id).attr('data-menutitle', title)
+		}
+
+		this.setData = function(data) {
+			if ('title' in data) {
+				$('.' + id).attr('data-menutitle', data.title)
+			}
+		}
 	},
 	$events: 'contextmenuchange'
 });
