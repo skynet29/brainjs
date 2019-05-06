0acae72683bba1faf16cca6abea354045e31fdcf
Marc DELOMEZ <marc.delomez@free.fr>
2019-05-06 15:29:03 +0200

    refactor pdf ctrl to use leaflet map

--- src/ext/pdf.js
+++ src/ext/pdf.js
@@ -9,7 +9,26 @@ $$.control.registerControl('brainjs.pdf', {
 
 		const {worker} = this.props
 
-		const ctrl = $$.viewController(elt)
+		const div = $('<div>').css('height', '100%').appendTo(elt).get(0)
+
+		const map = L.map(div, {
+			crs: L.CRS.Simple,
+			attributionControl: false,
+			zoomControl: false,
+			minZoom: -4
+		})
+
+		let bounds
+		let imageOverlay = null
+
+		function load(url) {
+			const image = new Image()
+			image.onload = function() {
+
+
+			}
+			image.src = url			
+		}		
 
 		const pdfjsLib = window['pdfjs-dist/build/pdf']
 
@@ -17,10 +36,10 @@ $$.control.registerControl('brainjs.pdf', {
 
 		elt.css({'text-align': 'center', width: '100%'})
 
-		const canvas = $('<canvas>').appendTo(elt).get(0)
+		const canvas = $('<canvas>').get(0)
 		const canvasContext = canvas.getContext('2d')
 		let pdfDoc = null
-		let scale = 1
+		let scale = 1.5
 		let currentPage = 1
 
 
@@ -30,7 +49,7 @@ $$.control.registerControl('brainjs.pdf', {
 
 			return pdfDoc.getPage(pageNo).then((page) => {
 				const viewport = page.getViewport(scale)
-				console.log('viewport', viewport)
+				//console.log('viewport', viewport)
 				canvas.width = viewport.width
 				canvas.height = viewport.height
 
@@ -38,16 +57,24 @@ $$.control.registerControl('brainjs.pdf', {
 					canvasContext,
 					viewport
 				}).then(() => {
+					const dataUrl = canvas.toDataURL('image/png')
+
+					//console.log('width: ', width, ' height:', height)
+					bounds = [[0, 0], [canvas.height, canvas.width]]
+					if (imageOverlay == null) {
+						imageOverlay = L.imageOverlay(dataUrl, bounds).addTo(map)					
+					}
+					else {
+						imageOverlay.setBounds(L.latLngBounds(bounds))
+						imageOverlay.setUrl(dataUrl)
+					}
+					map.fitBounds(bounds)					
 					return pageNo
 				})
 
 			})
 		}
 
-		this.setZoomLevel = function(zoomLevel) {
-			scale = zoomLevel
-			return renderPage(currentPage)
-		}
 
 		this.nextPage = function() {
 			if (currentPage < pdfDoc.numPages) {
@@ -66,7 +93,7 @@ $$.control.registerControl('brainjs.pdf', {
 		}
 
 		this.openFile = function(url) {
-			console.log('[pdf] openFile', url)
+			//console.log('[pdf] openFile', url)
 			return pdfjsLib.getDocument(url).then((pdfDoc_) => {
 				pdfDoc = pdfDoc_
 				return renderPage(currentPage).then(() => {
@@ -75,13 +102,19 @@ $$.control.registerControl('brainjs.pdf', {
 			})
 		}
 
+		this.fit = function() {
+			if (bounds != undefined) {
+				map.fitBounds(bounds)
+			}
+		}
+
 	},
 
 	$iface: `
 		openFile(url):Promise(numPages);
 		prevPage():Promise(currentPage);
 		nextPage():Promise(currentPage);
-		setZoomLevel(zoomLevel):Promise
+		fit()
 	`
 
 
