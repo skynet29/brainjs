5b78542bd1fe500b3601eb5a52c90eba83ec741f
Marc DELOMEZ <marc.delomez@free.fr>
2019-06-22 15:42:25 +0200

    manage disabled items menu in contextmenu ctrl

--- src/lib/controls/contextmenu.js
+++ src/lib/controls/contextmenu.js
@@ -7,12 +7,24 @@ $$.control.registerControl('brainjs.contextmenu', {
 	},
 	init: function(elt) {
 
-		const {items, trigger, title, iconColorCls} = this.props
+		let {items, trigger, title, iconColorCls} = this.props
 
 		const id = elt.attr('id')
 		let className = id
 		if (title !== false) {
 			className += ' data-title'
+		}
+
+		const kItems = $.extend(true, {}, items)
+		for(let k in items) {
+
+			kItems[k].disabled = function() {	
+				//console.log('disabled', k, items[k])	
+				if (items[k].disabled == undefined)	{
+					return false
+				}	
+				return items[k].disabled
+			}
 		}
 
 		$.contextMenu({
@@ -24,11 +36,15 @@ $$.control.registerControl('brainjs.contextmenu', {
 			callback: function(cmd) {
 				elt.trigger('contextmenuchange', {cmd})
 			},
-			items
+			items: kItems
 		})
 		$(`.${id} .context-menu-item`).removeClass('context-menu-icon')	
+
+		const $i = $(`.${id} .context-menu-item > i`)
+		$i.addClass('fa-fw')
+
 		if (iconColorCls !== false) {
-			$(`.${id} .context-menu-item > i`).addClass(iconColorCls)	
+			$i.addClass(iconColorCls)	
 		}
 
 		if (title != '') {
@@ -36,8 +52,12 @@ $$.control.registerControl('brainjs.contextmenu', {
 		}
 
 		this.setData = function(data) {
+			//console.log('[contextmenu] setData', data)
 			if ('title' in data) {
 				$('.' + id).attr('data-menutitle', data.title)
+			}
+			if ('items' in data) {
+				items = data.items
 			}
 		}
 	},
