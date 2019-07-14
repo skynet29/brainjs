01704769d33f6f56205e7a84b667617c45dd376f
Marc DELOMEZ <marc.delomez@free.fr>
2019-07-14 15:55:09 +0200

    update flipswitch ctrl

--- src/lib/controls/flipswitch.js
+++ src/lib/controls/flipswitch.js
@@ -14,16 +14,35 @@ $$.control.registerControl('brainjs.flipswitch', {
 
 		const {height, width, texts} = this.props
 
+		let value = elt.val()
+		if (value == '') {
+			value = texts.right
+		}
+		console.log('value', value)
+		const init = (value == texts.left ? 'right' : 'left')
+
 		elt.flipswitch({
 			texts,
 			height, 
-			width
+			width,
+			init
 		})
 		.on('change', function(ev, settings, state) {
 			//console.log('change', settings, state)
 			state = (state == 'left') ? 'right' : 'left'
-			elt.trigger('flipswitchchange', settings.texts[state])
+			value = settings.texts[state]
+			elt.trigger('flipswitchchange', value)
 		})
+
+		this.getValue = function() {
+			console.log('getValue', value)
+			return value
+		}
+
+		this.setValue = function(val) {
+			console.log('setValue', val)
+			value = val
+		}
 	},
 	$events: 'flipswitchchange'
 
