29d7b7ad3c4d457b35b94a86a35c90e304d5ea26
Marc DELOMEZ <marc.delomez@free.fr>
2020-06-19 17:46:58 +0200

    add print function for pdf ctrl

--- src/ext/pdf.js
+++ src/ext/pdf.js
@@ -96,6 +96,64 @@ $$.control.registerControl('brainjs.pdf', {
 			}
 		}
 
+		this.print = async function(options) {
+
+			options = options || {}
+			const title = options.title || ''
+			const canvas = document.createElement('canvas')
+			const canvasContext = canvas.getContext('2d')
+
+			
+			const ifr = document.createElement('iframe')
+			ifr.src = 'about:blank'
+			ifr.style = 'height: 0px; width: 0px; position: absolute'
+			document.body.appendChild(ifr)
+
+			const html = []
+			html.push(`<html><head><title>${title}</title></head>`)
+			html.push('<body>')
+			html.push(`
+				<style>
+					@media print {
+						@page {
+							margin: 1cm;
+						}
+					}
+				</style>
+			`)
+		
+			for(let i = 1; i <= pdfDoc.numPages; i++) {
+				const page = await pdfDoc.getPage(i)
+				const viewport = page.getViewport(scale)
+				//console.log('viewport', viewport)
+				canvas.width = viewport.width
+				canvas.height = viewport.height
+	
+	
+				await page.render({
+					canvasContext,
+					viewport
+				})
+	
+				html.push(`<img src=${canvas.toDataURL('image/png')}>`)
+			}
+			
+			html.push('<body>')
+			const documentToWriteTo = ifr.contentWindow.document
+			documentToWriteTo.open()
+			documentToWriteTo.write(html.join(''))
+			documentToWriteTo.close()
+	
+		
+			ifr.focus()
+			setTimeout(() => {
+			  ifr.contentWindow.print()
+			  ifr.parentElement.removeChild(ifr)
+			}, 200)
+			
+		
+		}
+
 	},
 
 	$iface: `
--- src/lib/core/jquery-ext.js
+++ src/lib/core/jquery-ext.js
@@ -1,200 +1,200 @@
-(function(){
+(function () {
 
 
-$.fn.bnFind= function(selector) {
+  $.fn.bnFind = function (selector) {
     return this.find(selector).add(this.filter(selector))
-}
+  }
 
-$.fn.bnFindAttr= function(attrName, cbk) {
-    this.bnFind(`[${attrName}]`).each(function() {
+  $.fn.bnFindAttr = function (attrName, cbk) {
+    this.bnFind(`[${attrName}]`).each(function () {
       const elt = $(this)
       const value = elt.attr(attrName)
       elt.removeAttr(attrName)
       cbk(elt, value)
     })
     return this
-}
+  }
 
-$.fn.setClass = function(data) {
-  for(let className in data) {
+  $.fn.setClass = function (data) {
+    for (let className in data) {
 
-    const isActive = data[className]
+      const isActive = data[className]
 
-    if (isActive) {
-      this.addClass(className)
+      if (isActive) {
+        this.addClass(className)
+      }
+      else {
+        this.removeClass(className)
+      }
+    }
+    return this
+
+  }
+
+  $.fn.setVisible = function (isVisible) {
+    if (isVisible) {
+      this.show()
     }
     else {
-      this.removeClass(className)
-    }    
-  }
-  return this
-
-}
-
-$.fn.setVisible = function(isVisible) {
-  if (isVisible) {
-    this.show()
-  }
-  else {
-    this.hide()
-  }
-
-  return this
-}
-
-$.fn.iface = function() {
-  return this.get(0) && this.get(0).ctrl
-}
-
-$.fn.setProp = function(data) {
-
-  const iface = this.iface()
-
-  if (iface && typeof iface.setProp == 'function') {
-    iface.setProp.call(iface, data)
-    return
-  }    
-
-
-  for(let propName in data) {
-
-    const value = data[propName]
-
-    if (this.hasClass('ui-button') && propName == 'disabled') {
-      this.button('option', 'disabled', value )
-    }
-    else {
-      this.prop(propName, value)
+      this.hide()
     }
 
+    return this
   }
 
-}
-
-$.fn.setData = function(data) {
-  //console.log('setData', data)
-  const iface = this.iface()
-
-  if (iface && typeof iface.setData == 'function') {
-    iface.setData.call(iface, data)
+  $.fn.iface = function () {
+    return this.get(0) && this.get(0).ctrl
   }
-  else {
-    this.data(data)
-  }
-}
 
-$.fn.setValue = function(value) {
-  if (this.get(0).tagName == 'INPUT' && this.attr('type') == 'checkbox') {
-    this.prop('checked', value)
-    return
-  }  
-  const iface = this.iface()
+  $.fn.setProp = function (data) {
 
-  if (iface && typeof iface.setValue == 'function') {
-    iface.setValue(value)
-  }
-  else {
-    this.val(value)
-  }
-}
+    const iface = this.iface()
 
-$.fn.getValue = function() {
-  const type = this.attr('type')
-  if (this.get(0).tagName == 'INPUT' && type == 'checkbox') {
-    return this.prop('checked')
-  }    
-  const iface = this.iface()
-  if (iface && typeof iface.getValue == 'function') {
-    return iface.getValue()
-  }
-  var ret = this.val()
-
-  if (type == 'number' || type == 'range') {
-    ret = parseFloat(ret)
-  }
-  return ret
-}
-
-$.fn.getFormData = function() {
-  var ret = {}
-  this.find('[name]').each(function() {
-    var elt = $(this)
-    var name = elt.attr('name')
-    ret[name] = elt.getValue()
-
-  })
-
-  return ret
-}
-
-$.fn.resetForm = function() {
-  if (this.get(0).tagName == "FORM") {
-    this.get(0).reset()
-  }   
-}
-
-$.fn.setFormData = function(data) {
-
-  //console.log('setFormData', data)
-  //this.resetForm()
-
-  for(var name in data) {
-    var value = data[name]
-    var elt = this.find(`[name=${name}]`)
-    if (elt.length) {
-      elt.setValue(value)       
+    if (iface && typeof iface.setProp == 'function') {
+      iface.setProp.call(iface, data)
+      return
     }
 
-  
-  }
 
-  return this
-}
+    for (let propName in data) {
 
-$.fn.safeEmpty = function() {
-  this.bnFind('.CustomControl').each(function() {
-    const iface = $(this).iface()
-    //console.log('remove ctrl', iface)
+      const value = data[propName]
 
-    if (typeof iface.dispose == 'function') {
-      iface.dispose()
-    }
-  })
-
-  this.empty()
-
-  return this
-}
-
-$.fn.addControl = function(ctrlName, data, events) {
-  //console.log('$.addControl', ctrlName, data)
-  var newCtrl = $('<div>', {style: 'height: 100%;overflow: hidden;'})
-  this.append(newCtrl) 
-  if (data) {
-    newCtrl.data(data)
-  }
-  if (typeof events == 'object') {
-    for(let evName in events) {
-      let fn  = events[evName]
-      if (typeof fn == 'function') {        
-        const [name, selector] = evName.split('.')
-
-        if (selector != undefined) {
-          newCtrl.on(name, '.' + selector, fn)
-        }
-        else {
-          newCtrl.on(name, fn)
-        }                
+      if (this.hasClass('ui-button') && propName == 'disabled') {
+        this.button('option', 'disabled', value)
+      }
+      else {
+        this.prop(propName, value)
       }
 
     }
-  }
-  $$.control.createControl(ctrlName, newCtrl)
-  return newCtrl
-}
 
-$.fn.setItems = function(items) {
-  return this.empty().append(items.map((item) => {
+  }
+
+  $.fn.setData = function (data) {
+    //console.log('setData', data)
+    const iface = this.iface()
+
+    if (iface && typeof iface.setData == 'function') {
+      iface.setData.call(iface, data)
+    }
+    else {
+      this.data(data)
+    }
+  }
+
+  $.fn.setValue = function (value) {
+    if (this.get(0).tagName == 'INPUT' && this.attr('type') == 'checkbox') {
+      this.prop('checked', value)
+      return
+    }
+    const iface = this.iface()
+
+    if (iface && typeof iface.setValue == 'function') {
+      iface.setValue(value)
+    }
+    else {
+      this.val(value)
+    }
+  }
+
+  $.fn.getValue = function () {
+    const type = this.attr('type')
+    if (this.get(0).tagName == 'INPUT' && type == 'checkbox') {
+      return this.prop('checked')
+    }
+    const iface = this.iface()
+    if (iface && typeof iface.getValue == 'function') {
+      return iface.getValue()
+    }
+    var ret = this.val()
+
+    if (type == 'number' || type == 'range') {
+      ret = parseFloat(ret)
+    }
+    return ret
+  }
+
+  $.fn.getFormData = function () {
+    var ret = {}
+    this.find('[name]').each(function () {
+      var elt = $(this)
+      var name = elt.attr('name')
+      ret[name] = elt.getValue()
+
+    })
+
+    return ret
+  }
+
+  $.fn.resetForm = function () {
+    if (this.get(0).tagName == "FORM") {
+      this.get(0).reset()
+    }
+  }
+
+  $.fn.setFormData = function (data) {
+
+    //console.log('setFormData', data)
+    //this.resetForm()
+
+    for (var name in data) {
+      var value = data[name]
+      var elt = this.find(`[name=${name}]`)
+      if (elt.length) {
+        elt.setValue(value)
+      }
+
+
+    }
+
+    return this
+  }
+
+  $.fn.safeEmpty = function () {
+    this.bnFind('.CustomControl').each(function () {
+      const iface = $(this).iface()
+      //console.log('remove ctrl', iface)
+
+      if (typeof iface.dispose == 'function') {
+        iface.dispose()
+      }
+    })
+
+    this.empty()
+
+    return this
+  }
+
+  $.fn.addControl = function (ctrlName, data, events) {
+    //console.log('$.addControl', ctrlName, data)
+    var newCtrl = $('<div>', { style: 'height: 100%;overflow: hidden;' })
+    this.append(newCtrl)
+    if (data) {
+      newCtrl.data(data)
+    }
+    if (typeof events == 'object') {
+      for (let evName in events) {
+        let fn = events[evName]
+        if (typeof fn == 'function') {
+          const [name, selector] = evName.split('.')
+
+          if (selector != undefined) {
+            newCtrl.on(name, '.' + selector, fn)
+          }
+          else {
+            newCtrl.on(name, fn)
+          }
+        }
+
+      }
+    }
+    $$.control.createControl(ctrlName, newCtrl)
+    return newCtrl
+  }
+
+  $.fn.setItems = function (items) {
+    return this.empty().append(items.map((item) => {
       const option = $('<option>')
 
       if (typeof item == 'string') {
@@ -207,26 +207,26 @@ $.fn.setItems = function(items) {
         }
       }
       return option
-  }))
-}
+    }))
+  }
 
 
-$.fn.isEqual = function($otherSet) {
-  if (this === $otherSet) return true
-  if (this.length != $otherSet.length) return false
-  var ret = true
-  this.each(function(idx) { 
-    if (this !== $otherSet[idx]) {
-       ret = false; return false;
-     }
-    
-  })
-  return ret;
-}
+  $.fn.isEqual = function ($otherSet) {
+    if (this === $otherSet) return true
+    if (this.length != $otherSet.length) return false
+    var ret = true
+    this.each(function (idx) {
+      if (this !== $otherSet[idx]) {
+        ret = false; return false;
+      }
 
-$.fn.scrollToBottom = function() {
-  return this.scrollTop(this.get(0).scrollHeight)
-}
+    })
+    return ret;
+  }
+
+  $.fn.scrollToBottom = function () {
+    return this.scrollTop(this.get(0).scrollHeight)
+  }
 
 
 })();
